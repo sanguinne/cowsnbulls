b02760b25ae6019c6d7066cb055ffcd1c6aef60f
sanguinne <earlyymorning@gmail.com>
2017-05-28 12:25:41 +0200

    Add function to enteruser's number

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void input(int* user);
+void comp_number (int* num);
 
 int main() {
     printf("COWS'N'BULLS\n");
     printf("Правила игры:\n");
     printf("Компьютер загадывает четырехзначное число, цифры в котором не повторяются. Твоя задача - отгадать это число. После каждого ввода числа приложение выведет число быков и коров. Быки - количество верно угаданных цифр, стоящих на верных позициях. Коровы - верно угаданные цифры, стоящие на неверных позициях. Удачи!\n");
-    int number[4];
+    int number[4], player[4];
     comp_number (&number);
     printf("Компьютер загадал число.\n");
+    input(player);
     return 0;
 }
 void comp_number (int* num) {
@@ -26,4 +31,10 @@ void comp_number (int* num) {
             }
         }
     }
+}
+void input(int* user) {
+     int i;
+     for (i = 0; i < 4; i++) {
+        scanf("%d", &user[i]);
+     }
 }
