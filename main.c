a0cc786eee3708b8e52e5e13995c7fb3fb28b0e1
sanguinne <earlyymorning@gmail.com>
2017-05-29 15:12:03 +0200

    Add validation of user's number

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 
 void input(int* user);
 void comp_number (int* num);
+int isnumbercorrect (int i);
 
 int main() {
     printf("COWS'N'BULLS\n");
     printf("Правила игры:\n");
     printf("Компьютер загадывает четырехзначное число, цифры в котором не повторяются. Твоя задача - отгадать это число. После каждого ввода числа приложение выведет число быков и коров. Быки - количество верно угаданных цифр, стоящих на верных позициях. Коровы - верно угаданные цифры, стоящие на неверных позициях. Удачи!\n");
     int number[4], player[4];
-    comp_number (&number);
-    printf("Компьютер загадал число.\n");
+    comp_number (number);
+    printf("Компьютер загадал число. Твой ход.\n");
     input(player);
     return 0;
 }
@@ -34,7 +35,25 @@ void comp_number (int* num) {
 }
 void input(int* user) {
      int i;
-     for (i = 0; i < 4; i++) {
-        scanf("%d", &user[i]);
+     scanf("%d", &i);
+     while (isnumbercorrect(i)==0) { 
+         printf("Число некорректно. Попробуй еще раз.\n");
+         scanf("%d", &i);
      }
+}
+int isnumbercorrect (int i) {
+     int arr[10] = {0};
+     if(i/1000 > 9 || i/1000 < 1) {
+         return 0;
+     }
+     while (i) {
+         arr[i%10]++;
+         i /= 10;
+     }
+     for(i=0; i<10;i++) {
+         if (arr[i] > 1) { 
+             return 0;
+         }
+     }
+     return 1;
 }
